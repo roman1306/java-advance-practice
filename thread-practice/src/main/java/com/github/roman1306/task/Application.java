c95693b3cb75f9a3d62d3fc0e91c7664ce413c71
Roman Petrenko <rpetrenko51@gmail.com>
2020-10-21 11:37:41 +0400

    initial commit

---
+++ thread-practice/src/main/java/com/github/roman1306/task/Application.java
@@ -0,0 +1,8 @@
+package com.github.roman1306.task;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ thread-practice/src/main/java/com/github/roman1306/task/entity/SlotForReservation.java
@@ -0,0 +1,9 @@
+package com.github.roman1306.task.entity;
+
+import java.time.LocalDate;
+
+public class SlotForReservation {
+    private String ad;
+    private LocalDate date;
+    private String hotel;
+}
