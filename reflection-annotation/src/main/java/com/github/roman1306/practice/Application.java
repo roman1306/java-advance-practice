9ff922c98705971e14be2b2b28ff6f14b49834f7
Roman Petrenko <rpetrenko51@gmail.com>
2020-10-25 16:44:13 +0400

    add reflection print

--- reflection-annotation/src/main/java/com/github/roman1306/practice/Application.java
+++ reflection-annotation/src/main/java/com/github/roman1306/practice/Application.java
@@ -1,7 +1,31 @@
 package com.github.roman1306.practice;
 
-import com.github.roman1306.practice.entity.Entity;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 
 public class Application {
-    Entity entity = new Entity();
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        Class<?> entityClass = Class.forName("com.github.roman1306.practice.entity.Entity");
+
+        for (Method method : entityClass.getDeclaredMethods()) {
+            System.out.println(method.getName());
+
+            System.out.println("\tParameters:");
+            for (Parameter parameter : method.getParameters()) {
+                System.out.println("\t\t" + parameter.toString());
+            }
+
+            System.out.println("\tAnnotations:");
+            for (Annotation annotation : method.getAnnotations()) {
+                System.out.println("\t\t" + annotation.toString());
+            }
+            System.out.println("\t\tParameters Annotation:");
+            for (AnnotatedType a: method.getAnnotatedParameterTypes()) {
+                System.out.println("\t\t\t" + a.getType());
+            }
+        }
+    }
 }
--- reflection-annotation/src/main/java/com/github/roman1306/practice/entity/Entity.java
+++ reflection-annotation/src/main/java/com/github/roman1306/practice/entity/Entity.java
@@ -5,8 +5,10 @@ import com.github.roman1306.practice.annotation.Secured;
 public class Entity {
 
     @Secured(number = 5)
-    public void someMethod() {}
+    public void someMethod(int number, String text) {}
 
     @Secured(number = 27, string = "text")
-    private void somePrivateMethod() {}
+    private void somePrivateMethod(String text) {}
+
+    void methodWithoutParameterAndAnnotation() {}
 }
