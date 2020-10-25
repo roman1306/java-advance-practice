3cb920e13cc6391d00230bb914ea3c30a1b88d08
Roman Petrenko <rpetrenko51@gmail.com>
2020-10-25 16:51:25 +0400

    -

--- regexp/src/main/java/com/github/roman1306/practice/Application.java
+++ regexp/src/main/java/com/github/roman1306/practice/Application.java
@@ -4,17 +4,23 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Application {
+
     public static void main(String[] args) {
         String text = "Notification has been sent 150 times to +4(351) 455 22 44 successfully. +1(431) 542 56 12 is unreachable";
+
+        System.out.println(handlerText(text));
+    }
+
+    private static String handlerText (String text) {
 
         Pattern handler = Pattern.compile("\\+\\d\\(\\d{3}\\)\\s\\d{3}\\s\\d{2}\\s\\d{2}");
         Matcher matcher = handler.matcher(text);
 
         while (matcher.find()) {
-            text = text.replace(matcher.group(), matcher.group().replaceAll("\\s+|\\(|\\)" ,""));
+            text = text.replace(matcher.group(), matcher.group().replaceAll("\\+|\\s+|\\(|\\)" ,""));
         }
 
-        System.out.println(text);
+        return text;
     }
 
 }
