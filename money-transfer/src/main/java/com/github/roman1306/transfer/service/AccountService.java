fbd5037a44d8a6abecb5bf6f5596c4f3ed940368
Roman Petrenko <rpetrenko51@gmail.com>
2020-10-27 16:45:00 +0400

    Add logger SLF4J

--- money-transfer/src/main/java/com/github/roman1306/transfer/service/AccountService.java
+++ money-transfer/src/main/java/com/github/roman1306/transfer/service/AccountService.java
@@ -1,11 +1,11 @@
 package com.github.roman1306.transfer.service;
 
 import com.github.roman1306.transfer.entity.Account;
-
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AccountService {
-    private static Logger logger = Logger.getLogger(AccountService.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger("logger");
 
     public static Account createAccount(String id, long balance) {
         return new Account(id, balance);
@@ -13,21 +13,19 @@ public class AccountService {
 
     public static boolean withdrawalAndReplenishment(Account from, Account to, long sum) {
         if (from.equals(to)) {
-            logger.warning("Operation failed: accounts equal");
+            logger.error("Operation failed: accounts equal {} = {} ", from.getId(), to.getId());
             return false;
         }
 
-        logger.info("Operation begin from " + from.getId() +
-                " to " + to.getId() + " summa " + sum);
+        logger.info("Operation begin from {} to {} summa {}", from.getId(), to.getId(), sum);
 
         if (from.getBalance() < sum) {
-            logger.warning("Operation failed: account id " + from.getId()
-                    + " not enough balance " + from.getBalance());
+            logger.error("Operation failed: not enough balance = {} summa = {}", to.getId(), sum);
             return false;
         } else {
             from.setBalance(from.getBalance() - sum);
             to.setBalance(to.getBalance() + sum);
-            logger.info("Operation successful");
+            logger.info("Operation successful balance from = {} balance to = {}", from.getBalance(), to.getBalance());
             return true;
         }
 
